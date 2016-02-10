b37b656f7803dd25050e4a90c65e18eb66a6ffa3
Jasmine <jasmine.tlkdr@gmail.com>
2016-02-10 16:34:16 +0000

    fixed css

--- src/test.py
+++ src/test.py
@@ -4,6 +4,11 @@ from flask import render_template
 from subprocess import check_output
 
 app = Flask(__name__)
+def get_resource_as_string(name, charset='utf-8'):
+    with app.open_resource(name) as f:
+        return f.read().decode(charset)
+
+app.jinja_env.globals['get_resource_as_string'] = get_resource_as_string
 
 @app.route("/")
 def my_form():
