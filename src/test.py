0650bfa381ead99218745c2c587966bf29c9cc15
Jasmine Talukder <Jasmine@Jasmines-MacBook-Pro.local>
2016-02-17 11:44:04 +0000

    login page added

--- src/test.py
+++ src/test.py
@@ -32,5 +32,9 @@ def signUp():
     return render_template("register.html")
     # do something
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port="8000")
