2465468dcdf190c732ac455bcf9dfb9ac82b94ff
Luke Grehan <luketenant@gmail.com>
2016-02-19 18:06:58 +0000

    integrate app.py

--- src/test.py
+++ src/test.py
@@ -1,17 +1,24 @@
-from flask import Flask
-from flask import request
-from flask import render_template
-from subprocess import check_output
-from subprocess import STDOUT
-from subprocess import CalledProcessError
+from flask import Flask, request, render_template, request, redirect, url_for, send_from_directory
+from werkzeug import secure_filename
+from subprocess import check_output, STDOUT, CalledProcessError
 import tempfile
+import os
 
+DEBUG = True
 app = Flask(__name__)
+
 def get_resource_as_string(name, charset='utf-8'):
     with app.open_resource(name) as f:
         return f.read().decode(charset)
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
+
 app.jinja_env.globals['get_resource_as_string'] = get_resource_as_string
+app.config['UPLOAD_FOLDER'] = 'uploads/'
+app.config['ALLOWED_EXTENSIONS'] = set(['pml'])
+
 
 @app.route("/")
 def my_form():
@@ -34,5 +41,21 @@ def my_form_post():
 def ace():
     return render_template("form2.html")
 
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files['file']
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+# @app.route('/uploads/<filename>')
+# def uploaded_file(filename):
+#     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", port=8000)# debug=True)
+	app.run(host="0.0.0.0", port=8000, debug=DEBUG)
+
