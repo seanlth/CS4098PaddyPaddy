1125c9f193cb8d310e66bcf49a3664694a2a939d
seanlth <seanlthcamera@gmail.com>
2016-03-11 17:26:38 +0000

    adding editor saving

--- src/static/js/pml_generator.js
+++ src/static/js/pml_generator.js
@@ -1,17 +1,44 @@
-function add_requirements(requirements, current_indentation) {
-	return "";
+function save_generated_pml(pml, callback){
+    var content = btoa(pml); // encode editor content
+    $.ajax({
+        url: '/tmp',
+        data: { content: content },
+        type: 'POST',
+        success: callback,
+        error: function(error) {
+            console.log(error);
+        }
+    });
 }
 
-function add_provisions(provisions, current_indentation) {
-	return "";
+function add_requirements(requirements, current_indentation) {
+    if ( requirements != "" ) {
+	    var requirements_string = current_indentation + "requires { \n";
+        requirements_string += current_indentation + "    " + requirements + "\n";
+        requirements_string += current_indentation + "}";
+    
+        return requirements_string;
+    }
+    return "";
+}
+
+function add_provisions(provides, current_indentation) {
+    if ( provides != "" ) {
+	    var provides_string = current_indentation + "provides { \n";
+        provides_string += current_indentation + "    " + provides + "\n";
+        provides_string += current_indentation + "}";
+
+        return provides_string;
+    } 
+    return "";
 }
 
 // builds the action string
 function add_action(action, current_indentation) {
 	node = current_indentation + "action " + action.name + " { \n";
-	node += current_indentation + "    " + add_requirements(action.requirements, current_indentation + "    ");
-	node += current_indentation + "    " + add_provisions(action.provisions, current_indentation + "    ") + "\n"; 
-	node += current_indentation + "}"
+	node += add_requirements(action.requires, current_indentation + "    ") + "\n";
+	node += add_provisions(action.provides, current_indentation + "    ") + "\n"; 
+	node += current_indentation + "}";
 	
 	return node;
 }
@@ -53,6 +80,9 @@ function json_to_pml(program) {
 		
 	PML_code += add_primitives(program.actions, "    ") + "\n";
 	PML_code += "}"
+    
+    var content = btoa(PML_code) // encode content
+    save_generated_pml(content, function(response) {  } );     
 
 	return PML_code;
 }
