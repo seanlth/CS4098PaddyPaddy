12cd1215b33fdc7361dea282b7898f795bee1b53
seanlth <seanlthcamera@gmail.com>
2016-03-13 16:36:39 +0000

    autocomplete test - for some definition of test

--- src/tests.py
+++ src/tests.py
@@ -31,6 +31,9 @@ class FlaskrTestCase(unittest.TestCase):
     def logout(self):
         return self.app.get('/logout', follow_redirects=True) 
     
+    def editor(self):
+        return self.app.get('/', follow_redirects=True)  
+
     def test_register(self):
         print('testing register')
         rv = self.register('test', 'test')
@@ -41,12 +44,20 @@ class FlaskrTestCase(unittest.TestCase):
         rv = self.login('test', 'test')
         assert b'Incorrect/Invalid e-mail and/or password<br/>' != rv.data
 
-
     def test_email_auth_failure(self):
         print('testing login/logout failure')
         rv = self.login('none', 'none')
         assert b'Incorrect/Invalid e-mail and/or password<br/>' == rv.data
-    
+   
+    def test_auto_complete(self):
+        print('testing authcomplete')
+        rv = self.editor()
+        response = rv.data.decode()
+        liveautocomplete = "enableLiveAutocompletion: true"
+        basicautocomplete = "enableBasicAutocompletion: true"
+        assert response.find(liveautocomplete) != -1
+        assert response.find(basicautocomplete) != -1
+            
 
 if __name__ == '__main__':
     unittest.main()
