eca41dcd6c06bcc82a47a2c5baabe14a100ef71c
seanlth <seanlthcamera@gmail.com>
2016-03-13 15:55:57 +0000

    email auth failure

--- src/tests.py
+++ src/tests.py
@@ -5,18 +5,31 @@ import unittest
 import tempfile
 
 
-
 class FlaskrTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
-        flaskr.app.config['TESTING'] = True
-        self.app = flaskr.app.test_client()
-        flaskr.init_db()
+        self.db_fd, runServer.app.config['DATABASE'] = tempfile.mkstemp()
+        runServer.app.config['TESTING'] = True
+        self.app = runServer.app.test_client()
 
     def tearDown(self):
         os.close(self.db_fd)
-        os.unlink(flaskr.app.config['DATABASE'])
+        os.unlink(runServer.app.config['DATABASE'])
+    
+    def login(self, email, password):
+        return self.app.post('/login', data=dict(
+            email=email,
+            password=password
+        ), follow_redirects=True)
+
+    def logout(self):
+        return self.app.get('/logout', follow_redirects=True) 
+
+    def test_email_auth_failure(self):
+        print('testing login/logout')
+        rv = self.login('none', 'none')
+        assert b'Incorrect/Invalid e-mail and/or password<br/>' == rv.data
+
 
 if __name__ == '__main__':
     unittest.main()
